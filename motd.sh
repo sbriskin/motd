27f970a7d4fe81b04254e4e230b184af22d51d30
Sergey Briskin <sergey@briskin.org>
2020-01-26 18:41:20 +0300

    Minor fixes

--- motd.sh
+++ motd.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+# On CentOS and other OSes without systemd motd 
+# place this file in /etc/profile.d/ to activate dynamic MOTD 
+#
+# Do not use this file on Debian-based distros!
+#
 LC_ALL=en_US.UTF-8
 export LC_ALL
 /usr/bin/motd
